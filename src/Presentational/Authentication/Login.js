cdba261fc7a38710da511abc2b65d7943790061d
Minesh Patel <minesh.patel@net-a-porter.com>
2018-11-17 20:39:58 +0000

    Add background images for login page

--- src/Presentational/Authentication/Login.js
+++ src/Presentational/Authentication/Login.js
@@ -11,49 +11,51 @@ const Login = ({
     handleChange,
     data
 }) => (
-  <form onSubmit={handleSubmit}>
-    <div class="flex-container">
-      <div class="flex-item flex-item-login box-login">
-        <div class="box-wishlist">
-          <Image src={logo} className="home-logo" />
-        </div>
-        <div class="box-wishlist">
-          <Header as="h2" className="login-header" textAlign="center">
-            Login to Secret Santa
-          </Header>
-        </div>
-        <div class="box-wishlist one-col-span wishlist-input">
-          <Form.Input
-            fluid
-            icon='user'
-            iconPosition='left'
-            placeholder='Name'
-            name='memberName'
-            onChange={handleChange}
-          />
-        </div>
-        <div class="box-wishlist one-col-span wishlist-input">
-          <Form.Input fluid icon='group' iconPosition='left' placeholder='Group name' name='groupID' onChange={handleChange}/>
-        </div>
-        <div class="box-wishlist one-col-span wishlist-input">
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Code'
-            type='password'
-            name='passphrase'
-            onChange={handleChange}
-          />
-        </div>
-        <div class="box-wishlist one-col-span">
-          <Button color='teal' fluid size='large' className="login-btn">
-            Login
-          </Button>
+  <div className="container-login">
+    <form onSubmit={handleSubmit}>
+      <div class="flex-container">
+        <div class="flex-item flex-item-login box-login">
+          <div class="box-wishlist">
+            <Image src={logo} className="home-logo" />
+          </div>
+          <div class="box-wishlist">
+            <Header as="h2" className="login-header" textAlign="center">
+              Login to Secret Santa
+            </Header>
+          </div>
+          <div class="box-wishlist one-col-span wishlist-input">
+            <Form.Input
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='Name'
+              name='memberName'
+              onChange={handleChange}
+            />
+          </div>
+          <div class="box-wishlist one-col-span wishlist-input">
+            <Form.Input fluid icon='group' iconPosition='left' placeholder='Group name' name='groupID' onChange={handleChange}/>
+          </div>
+          <div class="box-wishlist one-col-span wishlist-input">
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Code'
+              type='password'
+              name='passphrase'
+              onChange={handleChange}
+            />
+          </div>
+          <div class="box-wishlist one-col-span">
+            <Button color='teal' fluid size='large' className="login-btn">
+              Login
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
 );
 
 Login.propTypes = {
