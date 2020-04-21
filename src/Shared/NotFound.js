8f113c4640d66dcb323cb85102e34c7bb609c486
Minesh Patel <minesh.patel@ynap.com>
2020-04-21 22:36:42 +0100

    Update 404 and 401 pages

--- src/Shared/NotFound.js
+++ src/Shared/NotFound.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Container from 'semantic-ui-react/dist/commonjs/elements/Container';
 import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import './ErrorPage.css';
 
 const NotFound = () => {
   document.body.className = 'error-page';
   return (
-    <div className="error-page-bg">
-      <Container text>
-        <Header as="h1" className="error-code-heading">404 Page Not Found!</Header>
-        <p className="error-description">Looks like you have strayed off-course, we are unable to find what you are looking for.</p>
-      </Container>
+    <div className="error-page-container error-page-bg">
+      <div className="wrapper">
+        <div className="box">
+          <Header as="h1" className="error-code-heading">404 Page Not Found!</Header>
+          <p className="error-description">Bah Humbug, looks like you are lost.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Shared/Unauthorised.js
+++ src/Shared/Unauthorised.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Container from 'semantic-ui-react/dist/commonjs/elements/Container';
 import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import './ErrorPage.css';
 
 const Unauthorised = () => {
   document.body.className = 'error-page';
   return (
-    <div className="error-page-bg">
-      <Container text>
-        <Header as="h1" className="error-code-heading">401 Unauthorised!</Header>
-        <p className="error-description">Bah Humbug, looks like you do not have permission to view the page.</p>
-      </Container>
+    <div className="error-page-container error-page-bg">
+      <div className="wrapper">
+        <div className="box">
+          <Header as="h1" className="error-code-heading">401 Unauthorised!</Header>
+          <p className="error-description">Bah Humbug, looks like you do not have permission to view the page.</p>
+        </div>
+      </div>
     </div>
   );
 };
